5642bfc623178c0a700a2fa96deddaf378a249f7
pete <peteward44@gmail.com>
2015-08-13 23:40:21 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # browserify-casesensitiverequire
 A simple browserify transform to ensure that the case of require()'d file names are consistent, and warn the user if they aren't. This makes sure that browersify code that is written on Windows will also work on *nix's.
+
+Example usage (command line)
+```
+npm i browserify-casesensitiverequire -g
+browserify -t browserify-casesensitiverequire ./myindex.js
+```
+
+I was originally tempted to create a transform that would automatically alter the case for you, however this seemed like bad practice so I have opted for emitting warnings instead.
+
+If the same file is require()'d by Browserify using different casing, on Windows this will cause the same file to be processed & compiled twice (which works, but is undesirable). On case-sensitive file systems, the operation will fail. So we have code that can build on Windows but not unix. This transform aims to resolve that, by warning the developer they have require()'d the same file at different points in their code using different casing.
---
+++ index.js
@@ -0,0 +1,41 @@
+/*jslint node: true */
+"use strict";
+
+var colors = require('colors/safe');
+var path = require( 'path' );
+var transformTools = require( 'browserify-transform-tools' );
+
+var paths = [];
+
+module.exports = transformTools.makeRequireTransform(
+	"browserify-casesensitiverequire",
+	{
+		evaluateArguments: true
+	},
+	function( args, opts, cb ) {
+
+		var include = args[0];
+		var fullpath = include;	
+		if ( opts.file && fullpath.match( /^\.{1,2}(\\|\/)/ ) ) {
+			// if a relative file path, create a full path for it so the same file can be referenced in from
+			// multiple locations and still be recognised as the same file
+			fullpath = path.normalize( path.join( path.dirname( opts.file ), fullpath ) );
+		}
+		// otherwise it's an absolute path or one that sits in node_modules
+		var lhs;
+		var exists = paths.some( function( p2 ) {
+			lhs = p2;
+			// checks if the full path of each file is the same, and shares the same casing
+			return fullpath.toUpperCase() === p2.fullpath.toUpperCase() && fullpath !== p2.fullpath;
+		} );
+		if ( exists ) {
+			console.error( colors.red( "browserify-casesensitiverequire: Inconsistency found" ) );
+			console.error( colors.cyan( "'" + include + "'" ), colors.yellow( "require'd from" ), colors.cyan( "'" + opts.file + "'" ) );
+			console.error( colors.yellow( "does not match case of" ) );
+			console.error( colors.cyan( "'" + lhs.include + "'" ), colors.yellow( "require'd from" ), colors.cyan( "'" + lhs.source + "'" ) );
+		} else {
+			paths.push( { include: include, fullpath: fullpath, source: opts.file } );
+		}
+		return cb();
+	}
+);
